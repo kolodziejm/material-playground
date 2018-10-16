26bca6c3119f30a2dfc5cf826ed3ccf9d72336af
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-10-16 18:47:37 +0200

    Created basic app bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global';
+
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from  '@material-ui/icons/Menu';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CssBaseline>
+          <AppBar position="fixed">
+            <Toolbar>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit">Material UI Playground</Typography>
+            </Toolbar>
+          </AppBar>
+        </CssBaseline>
       </div>
     );
   }
---
+++ src/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+createGlobalStyle`
+    body {
+        font-size: 62.5%;
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
