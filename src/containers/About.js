9d6537f00f8fe03c0a7600d07905c964875c3ba1
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-10-20 23:47:16 +0200

    Tweaked about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react'
 import Navbar from '../components/Navbar';
+import { Typography, withStyles, Grid, Paper, TextField, List, ListItem, Card, CardHeader, CardContent, Button } from '@material-ui/core';
 
-export default class About extends Component {
+const styles = {
+
+};
+
+class About extends Component {
     render() {
         return (
             <div>
                 <Navbar navValue={2} />
-                This is about page
+                <Grid container spacing={8}>
+                    <Grid item md={6} xs={12}>
+                        <Typography variant="h6" align="center">Example text one</Typography>
+                        <Card className={classes.card}>
+                            <CardContent className={classes.cardContent}>
+                                <Typography variant="body1">This is an example text of a card</Typography>
+                                <Button variant="contained" color="primary">Finish</Button>
+                            </CardContent>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardContent className={classes.cardContent}>
+                                <Typography variant="body1">This is an example text of a card</Typography>
+                                <Button variant="contained" color="primary">Finish</Button>
+                            </CardContent>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardContent className={classes.cardContent}>
+                                <Typography variant="body1">This is an example text of a card</Typography>
+                                <Button variant="contained" color="primary">Finish</Button>
+                            </CardContent>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardContent className={classes.cardContent}>
+                                <Typography variant="body1">This is an example text of a card</Typography>
+                                <Button variant="contained" color="primary">Finish</Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item md={6} xs={12}>
+                        <Typography variant="h6" align="center">Example text two</Typography>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
 }
+export default withStyles(styles)(About);
